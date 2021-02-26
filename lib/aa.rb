30340d152336e20c56cee95fdc485e64ca2a41f5
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-02-26 12:17:11 -0800

    Write test to recognize anagrams with different cases. Pass.

--- lib/aa.rb
+++ lib/aa.rb
@@ -6,6 +6,10 @@ class Word
   def aa_check()
     letters1 = @word1.downcase.gsub(/[!@#$%^&*().,:;'~`]/,'').gsub(/\s/,'').split("")
     letters2 = @word2.downcase.gsub(/[!@#$%^&*().,:;'~`]/,'').gsub(/\s/,'').split("")
-    letters2.all? { |letter| letters1.include?(letter) }
+    if letters2.all? { |letter| letters1.include?(letter) } 
+      "These two words are anagrams."
+    else
+      "These two words are not anagrams"
+    end
   end
 end
--- spec/aa_spec.rb
+++ spec/aa_spec.rb
@@ -3,20 +3,28 @@ require('aa.rb')
 require('pry')
 
 describe('Word#check') do
-  it("puts an input into an array with each letter as its own element, case insensitive return false") do
+  it("puts an input into an array with each letter as its own element, case insensitive") do
     words = Word.new("Wolf", "hello")
-    expect(words.aa_check).to(eq(false))
+    expect(words.aa_check).to(eq("These two words are not anagrams"))
   end
-  it("puts an input into an array with each letter as its own element and removes special characters or spaces and return false") do
+  it("puts an input into an array with each letter as its own element and removes special characters or spaces") do
     words = Word.new("Wolf! Pasta$", "hello")
-    expect(words.aa_check).to(eq(false))
+    expect(words.aa_check).to(eq("These two words are not anagrams"))
   end
-  it("puts two inputs into two different arrays with each letter as its own element and removes special characters or spaces and return false") do
+  it("puts two inputs into two different arrays with each letter as its own element and removes special characters or spaces") do
     words = Word.new("Wolf! Pasta$", "hello")
-    expect(words.aa_check).to(eq(false))
+    expect(words.aa_check).to(eq("These two words are not anagrams"))
   end
   it("returns true if all the letters in the first word match the letters in the second word") do
     words = Word.new("hello", "hello")
-    expect(words.aa_check).to(eq(true))
+    expect(words.aa_check).to(eq("These two words are anagrams."))
+  end
+  it("returns a confirmation string both words are anagrams") do
+    words = Word.new("elvis", "lives")
+    expect(words.aa_check).to(eq("These two words are anagrams."))
+  end
+  it("still correctly compares words even if they have different cases") do
+    words = Word.new("Elvis", "Lives")
+    expect(words.aa_check).to(eq("These two words are anagrams."))
   end
 end

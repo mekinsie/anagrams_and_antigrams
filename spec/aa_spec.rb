8fedb17ca64bf3bbbb9eb7e81729ac3c699eac1b
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-02-26 15:21:36 -0800

    Remove redundant tests.

--- spec/aa_spec.rb
+++ spec/aa_spec.rb
@@ -3,23 +3,7 @@ require('aa.rb')
 require('pry')
 
 describe('Word#check') do
-  it("puts an input into an array with each letter as its own element, case insensitive") do
-    words = Word.new("Wolf", "hello")
-    expect(words.aa_check).to(eq("These two words are not anagrams or antigrams"))
-  end
-  it("puts an input into an array with each letter as its own element and removes special characters or spaces") do
-    words = Word.new("Wolf! Pasta$", "hello")
-    expect(words.aa_check).to(eq("These two words are not anagrams or antigrams"))
-  end
-  it("puts two inputs into two different arrays with each letter as its own element and removes special characters or spaces") do
-    words = Word.new("Wolf! Pasta$", "hello")
-    expect(words.aa_check).to(eq("These two words are not anagrams or antigrams"))
-  end
-  it("returns true if all the letters in the first word match the letters in the second word") do
-    words = Word.new("hello", "hello")
-    expect(words.aa_check).to(eq("These two words are anagrams."))
-  end
-  it("returns a confirmation string both words are anagrams") do
+  it("returns a confirmation string if both words are anagrams") do
     words = Word.new("elvis", "lives")
     expect(words.aa_check).to(eq("These two words are anagrams."))
   end
@@ -43,5 +27,4 @@ describe('Word#check') do
     words = Word.new("Dormitory", "Dirty room")
     expect(words.aa_check).to(eq("These two words are anagrams."))
   end
-
 end

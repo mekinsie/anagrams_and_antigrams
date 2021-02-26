6f7f4d06bd2518c134cb7f033229d23e4b897883
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-02-26 11:32:39 -0800

    Write third test to return two arrays and alter previous tests to match. Pass.

--- lib/aa.rb
+++ lib/aa.rb
@@ -1,8 +1,11 @@
 class Word
-  def initialize(word)
-    @word = word
+  def initialize(word1, word2)
+    @word1 = word1
+    @word2 = word2
   end
-  def aa_check
-    letters = @word.downcase.gsub(/[!@#$%^&*().,:;'~`]/,'').gsub(/\s/,'').split("")
+  def aa_check()
+    letters1 = @word1.downcase.gsub(/[!@#$%^&*().,:;'~`]/,'').gsub(/\s/,'').split("")
+    letters2 = @word2.downcase.gsub(/[!@#$%^&*().,:;'~`]/,'').gsub(/\s/,'').split("")
+    return letters1, letters2
   end
 end
--- spec/aa_spec.rb
+++ spec/aa_spec.rb
@@ -3,12 +3,16 @@ require('aa.rb')
 require('pry')
 
 describe('Word#check') do
-  it("puts the input into an array with each letter as its own element, case insensitive") do
-    word = Word.new("Wolf")
-    expect(word.aa_check).to(eq(["w","o","l","f"]))
+  it("puts an input into an array with each letter as its own element, case insensitive") do
+    words = Word.new("Wolf", "hello")
+    expect(words.aa_check).to(eq([["w","o","l","f"],["h", "e", "l", "l", "o"]]))
   end
-  it("puts the input into an array with each letter as its own element and removes special characters or spaces") do
-    word = Word.new("Wolf! Pasta$")
-    expect(word.aa_check).to(eq(["w","o","l","f","p","a","s","t","a"]))
+  it("puts an input into an array with each letter as its own element and removes special characters or spaces") do
+    words = Word.new("Wolf! Pasta$", "hello")
+    expect(words.aa_check).to(eq( [["w", "o", "l", "f", "p", "a", "s", "t", "a"], ["h", "e", "l", "l", "o"]]))
+  end
+  it("puts two inputs into two different arrays with each letter as its own element and removes special characters or spaces") do
+    words = Word.new("Wolf! Pasta$", "hello")
+    expect(words.aa_check).to(eq( [["w", "o", "l", "f", "p", "a", "s", "t", "a"], ["h", "e", "l", "l", "o"]]))
   end
 end

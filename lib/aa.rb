7a59cdb33d3b4671d2592a16fbfa86b13e7f91f8
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-02-26 12:09:30 -0800

    Pass fourth test and update previous tests to return a boolean.

--- lib/aa.rb
+++ lib/aa.rb
@@ -6,8 +6,6 @@ class Word
   def aa_check()
     letters1 = @word1.downcase.gsub(/[!@#$%^&*().,:;'~`]/,'').gsub(/\s/,'').split("")
     letters2 = @word2.downcase.gsub(/[!@#$%^&*().,:;'~`]/,'').gsub(/\s/,'').split("")
-    letters1
-    letters2
     letters2.all? { |letter| letters1.include?(letter) }
   end
 end
--- spec/aa_spec.rb
+++ spec/aa_spec.rb
@@ -3,17 +3,17 @@ require('aa.rb')
 require('pry')
 
 describe('Word#check') do
-  it("puts an input into an array with each letter as its own element, case insensitive") do
+  it("puts an input into an array with each letter as its own element, case insensitive return false") do
     words = Word.new("Wolf", "hello")
-    expect(words.aa_check).to(include(["w","o","l","f"],["h", "e", "l", "l", "o"]))
+    expect(words.aa_check).to(eq(false))
   end
-  it("puts an input into an array with each letter as its own element and removes special characters or spaces") do
+  it("puts an input into an array with each letter as its own element and removes special characters or spaces and return false") do
     words = Word.new("Wolf! Pasta$", "hello")
-    expect(words.aa_check).to(include( ["w", "o", "l", "f", "p", "a", "s", "t", "a"], ["h", "e", "l", "l", "o"]))
+    expect(words.aa_check).to(eq(false))
   end
-  it("puts two inputs into two different arrays with each letter as its own element and removes special characters or spaces") do
+  it("puts two inputs into two different arrays with each letter as its own element and removes special characters or spaces and return false") do
     words = Word.new("Wolf! Pasta$", "hello")
-    expect(words.aa_check).to(include( ["w", "o", "l", "f", "p", "a", "s", "t", "a"], ["h", "e", "l", "l", "o"]))
+    expect(words.aa_check).to(eq(false))
   end
   it("returns true if all the letters in the first word match the letters in the second word") do
     words = Word.new("hello", "hello")

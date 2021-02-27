6b1b4e790e41da014cf82590933a5cc2850f3065
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-02-26 16:41:29 -0800

    Add script file to allow user to run program in terminal.

--- lib/script.rb
+++ lib/script.rb
@@ -1,1 +1,9 @@
-puts "Hello World!"
+#!/usr/bin/env ruby
+require_relative("aa.rb")
+
+puts "Please enter your first word"
+word1 = gets.chomp
+puts "Please enter your second word"
+word2 = gets.chomp
+words = Word.new(word1, word2)
+puts words.anagram

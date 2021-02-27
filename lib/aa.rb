2692917514e8925e94af2e45dc78f19c17314e6b
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-02-26 16:00:04 -0800

    Write test to return number of matching letters. Good fail.

--- lib/aa.rb
+++ lib/aa.rb
@@ -3,18 +3,18 @@ class Word
     @word1 = word1
     @word2 = word2
   end
-  def aa_check()
+  def anagram()
     letters1 = @word1.downcase.gsub(/[!@#$%^&*().,:;'~`]/,'').gsub(/\s/,'').split("")
     letters2 = @word2.downcase.gsub(/[!@#$%^&*().,:;'~`]/,'').gsub(/\s/,'').split("")
     
     if @word1.gsub(/[AEIOUaeiou]/, '').length == @word1.length || @word2.gsub(/[AEIOUaeiou]/, '').length == @word2.length
       "Please enter a valid word."
     elsif letters2.all? { |letter| letters1.include?(letter) }  
-      "These two words are anagrams."
+      "These words are anagrams."
     elsif letters2.all? { |letter| !letters1.include?(letter) }  
-      "These two words are not anagrams but are antigrams."
+      "These words are not anagrams but are antigrams."
     else
-      "These two words are not anagrams or antigrams"
+      "These words are not anagrams or antigrams"
     end 
   end  
 end
--- spec/aa_spec.rb
+++ spec/aa_spec.rb
@@ -2,29 +2,33 @@ require('rspec')
 require('aa.rb')
 require('pry')
 
-describe('Word#check') do
+describe('Word#anagram') do
   it("returns a confirmation string if both words are anagrams") do
     words = Word.new("elvis", "lives")
-    expect(words.aa_check).to(eq("These two words are anagrams."))
+    expect(words.anagram).to(eq("These words are anagrams."))
   end
   it("still correctly compares words even if they have different cases") do
     words = Word.new("Elvis", "Lives")
-    expect(words.aa_check).to(eq("These two words are anagrams."))
+    expect(words.anagram).to(eq("These words are anagrams."))
   end
   it("should check if the inputs are words") do
     words = Word.new("Htt", "Cdfld")
-    expect(words.aa_check).to(eq("Please enter a valid word."))
+    expect(words.anagram).to(eq("Please enter a valid word."))
   end
   it("should check if the words are antigrams if they are not anagrams") do
     words = Word.new("Spicy", "Mellow")
-    expect(words.aa_check).to(eq("These two words are not anagrams but are antigrams."))
+    expect(words.anagram).to(eq("These words are not anagrams but are antigrams."))
   end
   it("should check if inputs are neither antigrams or anagrams.") do
     words = Word.new("Hot", "Cold")
-    expect(words.aa_check).to(eq("These two words are not anagrams or antigrams"))
+    expect(words.anagram).to(eq("These words are not anagrams or antigrams"))
   end
   it("should account for multiple words in the inputs") do
     words = Word.new("Dormitory", "Dirty room")
-    expect(words.aa_check).to(eq("These two words are anagrams."))
+    expect(words.anagram).to(eq("These words are anagrams."))
+  end
+  it("should return how many letters from the argument are actual matches with the reciever") do
+    words = Word.new("Dormitory", "Dirty room")
+    expect(words.anagram).to(eq("These words are anagrams and 9 letters match."))
   end
 end
